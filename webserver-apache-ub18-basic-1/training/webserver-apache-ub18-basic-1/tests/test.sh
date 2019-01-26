5a7f849dd24cd23441c5582504e107857f017884
Marvin Heimbrodt <marvin@6uhrmittag.de>
2019-01-26 20:27:26 +0100

    use systemctl

--- webserver-apache-ub18-basic-1/training/webserver-apache-ub18-basic-1/tests/test.sh
+++ webserver-apache-ub18-basic-1/training/webserver-apache-ub18-basic-1/tests/test.sh
@@ -1,9 +1,8 @@
 #!/usr/bin/env bash
-
 apt-get  update
-apt install -y apache2
-service apache2 enable
-service apache2 start
+apt-get install -y apache2
+systemctl enable apache2
+systemctl start apache2
 echo "Hello World!" > /var/www/html/index.html
 a2enmod ssl
 a2ensite default-ssl.conf
