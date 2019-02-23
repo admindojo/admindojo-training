2921b68900d20c7769285328611d268379787e89
Marvin Heimbrodt <marvin@6uhrmittag.de>
2019-02-24 00:03:16 +0100

    change traning name structure to distro at the end

--- webserver-apache-ub18-basic-1/training/webserver-apache-ub18-basic-1/tests/test.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-apt-get  update
-apt-get install -y apache2
-systemctl enable apache2
-systemctl start apache2
-echo "Hello World!" > /var/www/html/index.html
-a2enmod ssl
-a2ensite default-ssl.conf
-systemctl reload apache2
